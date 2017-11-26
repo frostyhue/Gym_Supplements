ce8e200ebd1283cd40efbb4b4032a969987d9ff6
Rostislav Tinchev <rostislav.tinchev@gmail.com>
2017-11-26 20:48:33 +0100

    layout added

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>Gym Supplements</h1>
+@endsection
+
+@section('sidebar')
+
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <meta charset="utf-8">
+    <title>Gym Supplements</title>
+    <style media="screen">
+      html, body {
+        font-family: 'Raleway', sans-serif;
+        font-weight: 100;
+    }
+    </style>
+  </head>
+  <body>
+    @yield('content')
+    @section('sidebar')
+    <div class="sidebar">
+      <h3>Contents menu</h3>
+    </div>
+  </body>
+</html>
